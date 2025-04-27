9a080af7bb1b181a7bfbe8a7c432942e8a7c7b27
Hirak Chhatbar <hirak@goloti.com>
2025-04-27 14:41:18 +0530

    refactor: Improve code formatting and consistency in index.js and tej-env.js; update package version to 1.1.3

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const tejEnv = new TejEnv();
 tejEnv.init("utf-8");
 
 const env = (key) => {
-    return tejEnv.env(key);
-}
+  return tejEnv.env(key);
+};
 
 const setEnv = (key, value) => {
-    tejEnv.set(key, value);
-}
+  tejEnv.set(key, value);
+};
 
 const unsetEnv = (key) => {
-    tejEnv.unset(key);
-}
+  tejEnv.unset(key);
+};
 
-export {
-    env,
-    setEnv,
-    unsetEnv
-}
+const getAllEnv = () => {
+  return tejEnv.getAll();
+};
+
+export { env, setEnv, unsetEnv, getAllEnv };
